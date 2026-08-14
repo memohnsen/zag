3a24bd6e938b11769b77b8a155e155ce3f0f7dad
Maddisen <memohnsen@gmail.com>
2026-08-14 04:26:08 -0700

    nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Zag Zig development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, ... }:
+    let systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in { default = pkgs.mkShell {
+          packages = with pkgs; [ zig zls just git ];
+          shellHook = ''echo "Nix dev shell: zag"'';
+        }; });
+    };
+}
